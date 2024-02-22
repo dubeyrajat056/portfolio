873f411301f185500d3ac448f4f04c685f23aaf6
Rajat Dubey <153470158+dubeyrajat056@users.noreply.github.com>
2024-02-22 15:24:03 +0530

    add portfolio files

---
+++ try.js
@@ -0,0 +1,8 @@
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+    // You can add logic to handle form submission here
+    // For example, sending the form data to a server using AJAX
+    // and displaying a success message to the user
+    alert("Form submitted!");
+});
